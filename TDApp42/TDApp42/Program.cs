93538b72a88c0df4db44503d543695e97cf7153f
Cronje van Heerden <cronjeVH@CCHP.onmicrosoft.com>
2018-05-07 20:18:01 +0200

    Hello .. world

---
+++ TDApp42/TDApp42/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TDApp42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
